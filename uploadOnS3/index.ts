ea5d76521fb6d20e907acd6f62c084eba912b4f9
tommiii <36520669+tommiii@users.noreply.github.com>
2019-09-29 23:45:55 +0200

    code improved

--- uploadOnS3/index.ts
+++ uploadOnS3/index.ts
@@ -15,6 +15,7 @@ const { S3_BUCKET_NAME: Bucket, REGION: region } = process.env;
 
 exports.uploadOnS3 = async (event: APIGatewayEvent): Promise<any> => {
   if (!event.body) { return Response({ statusCode: 400, body: { message: "BadRequest" } }); }
+
   try {
     const { base64, key } = JSON.parse(event.body);
     const s3: AWS.S3 = new AWS.S3({ signatureVersion: "v4", region });
@@ -25,8 +26,12 @@ exports.uploadOnS3 = async (event: APIGatewayEvent): Promise<any> => {
       ContentEncoding: "base64",
       Key: key,
     };
-    await s3.putObject(obj).promise();
+    const response = await s3.putObject(obj).promise();
+    return Response({ statusCode: 200, body: response });
   } catch (err) {
+    if (err.statusCode === 403) {
+      return Response({ statusCode: 403, body: { message: "Access Denied" } });
+    }
     return Response({ statusCode: 500, body: { message: "InternalServerError" } });
   }
 };
