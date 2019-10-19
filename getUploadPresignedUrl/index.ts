365a6e693b4c60bae99deac7af1fbe631e76b389
tommiii <36520669+tommiii@users.noreply.github.com>
2019-10-19 16:57:04 +0200

    fix bucket and permission

--- getUploadPresignedUrl/index.ts
+++ getUploadPresignedUrl/index.ts
@@ -8,12 +8,12 @@ const { S3_BUCKET_NAME: Bucket, REGION: region } = process.env;
 
 exports.getUploadPresignedUrl = async (event: APIGatewayEvent): Promise<any> => {
   if (!event.body) { return Response({ statusCode: 400, body: { message: "BadRequest" } }); }
-
   try {
-    const { key: Key } = JSON.parse(event.body);
+    const { key: Key, type: ContentType } = JSON.parse(event.body);
     const s3: AWS.S3 = new AWS.S3({ signatureVersion: "v4", region });
-    const params = {Bucket, Key};
+    const params = { Bucket, Key, ContentType, Expires: 120 };
     const response = await s3.getSignedUrl('putObject', params);
+    console.log(response)
     return Response({ statusCode: 200, body: response });
   } catch (err) {
     error(err);
